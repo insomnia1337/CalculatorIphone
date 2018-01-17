4c9fdb58873b06281c15bc2cc853749f869380c0
insomnia1337 <mateusz.bieda95@gmail.com>
2018-01-17 11:55:13 +0100

    finally #01 calc

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import Calculator from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div id="wrapper">
+    <Calculator />
+  </div>,
+  document.getElementById("root")
+)
+
+  
